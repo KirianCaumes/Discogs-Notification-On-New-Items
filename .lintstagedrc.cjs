a75fe8172a60116d3dfa66e930f6bf5836596abf
KirianCaumes <kirian.caumes@gmail.com>
2024-07-17 22:31:44 +0200

    🐛 Fix cspell

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -3,6 +3,10 @@
  * @type {import('lint-staged').Config}
  */
 module.exports = {
-    '*.{ts,tsx,js,jsx,mjs}': ['prettier --ignore-unknown --check', 'eslint --max-warnings=0'],
-    '*.{html,json,svg,yml,xml}': ['prettier --ignore-unknown --check'],
+    '*.{ts,tsx,js,jsx,mjs}': [
+        'prettier --ignore-unknown --check',
+        'eslint --max-warnings=0',
+        'cspell --no-progress --dot --gitignore --no-must-find-files',
+    ],
+    '*.{html,json,svg,yml,xml}': ['prettier --ignore-unknown --check', 'cspell --no-progress --dot --gitignore --no-must-find-files'],
 }
